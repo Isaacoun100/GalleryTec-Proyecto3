9c44891655879b2ca6ec86ba2c256314d1294b10
Isaac Herrera Monge <Isaacoun100@gmail.com>
2021-11-25 02:47:48 -0600

    Testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+#include "ImageManagement/NewImage.h"
+#include "local/ReadWrite.h"
+
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
+    NewImage newImage;
+    ReadWrite readWrite;
+
+    string characters = newImage.characterList("local/example.ppm");
+
+    string numberList = newImage.frecuencyList("local/example.ppm",characters);
+
+    cout<<numberList<<endl;
+    cout<<characters<<endl;
+    return 0;
 
 }
