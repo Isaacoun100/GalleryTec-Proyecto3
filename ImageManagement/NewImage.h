99f04f572a331f606607650e20a9fdfb7965d872
Isaac Herrera Monge <Isaacoun100@gmail.com>
2021-11-26 06:56:49 -0600

    Classes commentated

--- ImageManagement/NewImage.h
+++ ImageManagement/NewImage.h
@@ -10,12 +10,49 @@ struct page;
 
 class NewImage {
 public:
+    /**
+     * This method will decompress the given image text usable text
+     * @param image
+     * @param id
+     * @return
+     */
     string decompressImage(string image, string id);
+
+    /**
+     * This method will create the dictionary that we need to compress and decompress
+     * @param path
+     * @return
+     */
     string createDictionary(string path);
+
+    /**
+     * This method receives an image text and compress image to a binary huffman compressed image text
+     * @param image
+     * @param id
+     * @return
+     */
     string compressImage(string image, string id);
 private:
+    /**
+     * Will return a string with the frecuency of the characters in the give string
+     * @param path
+     * @param characters
+     * @return
+     */
     string frecuencyList(string path, string characters);
+
+    /**
+     * Will delete duplicates and return the clean string
+     * @param path
+     * @return
+     */
     string characterList(string path);
+
+    /**
+     * Reads a given image from a path
+     * @param path
+     * @return
+     */
     string readImage(string path);
 };
 
--- huffman/HuffmanCompresion.h
+++ huffman/HuffmanCompresion.h
@@ -14,20 +14,104 @@ struct CompressedImage;
 
 class HuffmanCompresion {
 public:
+    /**
+     * Prints the contents of the heap
+     * @param root
+     * @param arr
+     * @param top
+     */
     void printCodes(struct MinHeapNode* root, int arr[], int top);
+
+    /**
+     * Initializes the huffman and returns the tree
+     * @param data
+     * @param freq
+     * @param size
+     * @return
+     */
     string HuffmanCodes(char data[], int freq[], int size);
+
+    /**
+     * Prints the tree into a more comprehensive  human readible way
+     * @param arr
+     * @param n
+     */
     void printArr(int arr[], int n);
 
 private:
+    /**
+     * This method creates the used dictionaty
+     * @param root
+     * @param result
+     * @param tmp
+     * @return
+     */
     string createDictionary(struct MinHeapNode* root, string result, string tmp);
+
+    /**
+     * This method initializes the tree and populates it
+     * @param data
+     * @param freq
+     * @param size
+     * @return
+     */
     struct MinHeapNode* buildHuffmanTree(char data[], int freq[], int size);
+
+    /**
+     * Inserts the given minHeap into the tree
+     * @param minHeap
+     * @param minHeapNode
+     */
     void insertMinHeap(struct MinHeap* minHeap,struct MinHeapNode* minHeapNode);
+
+    /**
+     * Creates the MinHeap and its leafs
+     * @param data
+     * @param freq
+     * @param size
+     * @return
+     */
     struct MinHeap* createAndBuildMinHeap(char data[], int freq[], int size);
+
+    /**
+     * The method receives 2 nodes and  swaps them
+     * @param a
+     * @param b
+     */
     void swapMinHeapNode(struct MinHeapNode** a,struct MinHeapNode** b);
+
+    /**
+     * Returns the minimun node from the tree
+     * @param minHeap
+     * @return
+     */
     struct MinHeapNode* extractMin(struct MinHeap* minHeap);
+
+    /**
+     * Sorts the elements from the tree
+     * @param minHeap
+     * @param idx
+     */
     void minHeapify(struct MinHeap* minHeap, int idx);
+
+    /**
+     * This method recieves a node and initializes it to the tree
+     * @param minHeap
+     */
     void buildMinHeap(struct MinHeap* minHeap);
+
+    /**
+     * Returns a boolean weather or not the hepa is size one
+     * @param minHeap
+     * @return
+     */
     int isSizeOne(struct MinHeap* minHeap);
+
+    /**
+     * If the root has no children returns true
+     * @param root
+     * @return
+     */
     int isLeaf(struct MinHeapNode* root);
 };
 
